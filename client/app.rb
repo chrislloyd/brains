c23243e65b924d0766094416ebd35c635d1d9e0c
Chris Lloyd <christopher.lloyd@gmail.com>
2009-10-17 10:38:16 +1100

    Suck on my chocolate salty balls Carl.

--- client/app.rb
+++ client/app.rb
@@ -2,8 +2,28 @@ require 'init'
 
 set :haml => {:format => :html5}
 
+helpers do
+  def json(obj)
+    content_type :text
+    obj.to_json
+  end
+end
+
 get '/' do
   haml :game
+end
+
+get '/map.json' do
+  json({
+    :width => 100,
+    :height => 100,
+    :scale => 40,
+    :key => {
+      'a' => '/images/a.gif',
+      'b' => '/images/b.gif'
+    },
+    :data => Array.new(100*100){ ['a','b'][rand(2)] }
+  })
 end
 
 get /^\/css\/(.+)\.css/ do |style_file|
