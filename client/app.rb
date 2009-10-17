0a30372da788e5f80bd0e23246c898cba47ad59a
Chris Lloyd <christopher.lloyd@gmail.com>
2009-10-17 16:50:34 +1100

    Game state for myles.

--- client/app.rb
+++ client/app.rb
@@ -26,6 +26,28 @@ get '/map.json' do
   })
 end
 
+get '/game.json' do
+  json({
+    :players => [
+      {
+        :name => 'Mutli',
+        :health => 85,
+        :energy => 30,
+        :direction => 0, # 0 => N, 1 => NE, 2 => E
+        :pos => {:x => 6, :y => 10}, # Game coords, not pixels
+        :state => :firing # standing, walking, dead
+      }
+    ],
+    :zombies => [
+      {
+        :direction => 0,
+        :pos => {:x => 4, :y => 10},
+        :state => :walking # attacking
+      }
+    ]
+  })
+end
+
 get /^\/css\/(.+)\.css/ do |style_file|
   sass_file = File.join('public','sass',"#{style_file}.sass")
   pass unless File.exist?(sass_file)
