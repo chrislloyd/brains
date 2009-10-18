78fc92883c3be03cd989cd4ddb889d47497851a5
Chris Lloyd <christopher.lloyd@gmail.com>
2009-10-18 17:31:30 +1100

    Basic client uploader.

---
+++ cli/examples/user.rb
@@ -0,0 +1,26 @@
+# MYLES YOUR GEM SUCKS BALLS
+# http://github.com/quackingduck/exemplor/issues#issue/2
+
+# gem 'exemplor'; require 'exemplor'
+# gem 'fakeweb'; require 'fakeweb'
+# gem 'fakefs'; require 'fakefs/safe'
+#
+# $: << File.join(File.dirname(File.expand_path(__FILE__)),'..','lib')
+# require 'brains'
+#
+# FakeWeb.register_uri(:get, 'http://game.local/keys/new', :body => 'secret_api_key')
+#
+# eg 'requesting a new key' do
+#   user = Brains::User.new
+#   user.request_new_key
+#   Check(user.key).is('secret_api_key')
+# end
+#
+# eg 'writing out the file' do
+#   user = Brains::User.new
+#
+#   Check(user)
+#
+# end
+#
+
---
+++ cli/templ/brain.rb
@@ -0,0 +1,2 @@
+def this_is_a_new_brain
+end
