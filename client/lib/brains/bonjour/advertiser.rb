e91bae07aee01c052c175b8bd22bdf04dc527463
Carl Woodward <cjwoodward@gmail.com>
2009-11-20 23:31:37 +1100

    Testing bounjour

--- client/lib/brains/bonjour/advertiser.rb
+++ client/lib/brains/bonjour/advertiser.rb
@@ -1,6 +1,22 @@
 require 'dnssd'
 
-def brain(opts={})
-  raise ArgumentError unless opts.has_key?(:name)
-  DNSSD::register("Brains #{opts[:name]}", "_http._tcp,_brains", nil, 4567)
+class Advertiser
+  attr_accessor :options, :name
+  
+  def initialize(options)
+    self.options = options
+    self.name = options[:name]
+  end
+  
+  def go!
+    register_app
+  end
+  
+  private
+    def register_app
+      STDOUT.puts "Registering #{Bananajour.web_uri}"
+      tr = DNSSD::TextRecord.new
+      tr["name"] = name
+      DNSSD.register("#{name}'s brain", "_http._tcp,_brains", nil, 4567, tr) {}
+    end
 end
