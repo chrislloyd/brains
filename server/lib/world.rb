abc39e19a8d9ee4d2d8a31a8b559ea05d23640a2
Chris Lloyd <christopher.lloyd@gmail.com>
2009-11-21 12:18:32 +1100

    You're gonna get got if you try to walk outside of the field.

--- server/lib/world.rb
+++ server/lib/world.rb
@@ -50,6 +50,8 @@ class World
   def try_to_place(actor, x, y)
     if actors.detect {|a| a != actor && a.x == x && a.y == y && !a.dead?}
       raise SteppingOnToesError
+    elsif !actor.is_a?(Zombie) && (x < 0 || y < 0 || x > width || y > height)
+      actor.hurt(20)
     else
       actor.x, actor.y = x, y
     end
