d18beb7f814642032214f5662705a410d08aefa4
Chris Lloyd <christopher.lloyd@gmail.com>
2009-11-21 12:40:18 +1100

    Helpful methods for clients.

--- client/lib/brains/helpers.rb
+++ client/lib/brains/helpers.rb
@@ -1,6 +1,27 @@
 require 'json'
 require 'brains/bonjour/advertiser'
 
+module Math
+  def self.min(x,y)
+    x < y ? x : y
+  end
+  def self.max(x,y)
+    x > y ? x : y
+  end
+end
+
+class Numeric
+  def to_deg
+    self * (180 / Math::PI)
+  end
+  def to_rad
+    self * (Math::PI / 180)
+  end
+  def near?(other, precision=1)
+    (other - precision) <= self && self <= (other + precision)
+  end
+end
+
 def brain(options)
   Advertiser.new(options).go!
 end
@@ -15,13 +36,17 @@ helpers do
     obj.to_json
   end
 
-  def direction_to(x1, y1, x2, y2)
+  def direction(x1, y1, x2, y2)
     (Math.atan2(x1-x2, y1-y2).to_deg + 180) % 360
   end
 
-  def distance_to(x1, y1, x2, y2)
-    (x1 - x2).abs + (y1 - y2).abs
+  alias :direction_to :direction
+
+  def distance(x1, y1, x2, y2)
+    Math.sqrt(((x1 - x2).abs ** 2) + ((y1 - y2).abs ** 2))
   end
+
+  alias :distance_to :distance
 
   def roll_dice(sides=6)
     rand(sides).zero?
